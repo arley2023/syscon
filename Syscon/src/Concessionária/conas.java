6a9b6f583f874ca15662f1ffc99296bde8fcf856
arley2023 <128830032+arley2023@users.noreply.github.com>
2024-09-18 11:13:48 -0300

    Update conas.java
    falta revisar

--- Syscon/src/Concessionária/conas.java
+++ Syscon/src/Concessionária/conas.java
@@ -22,6 +22,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.JComboBox;
@@ -37,14 +38,19 @@ public class conas {
 
 // armazena as consultas
 	private Statement statement = null;
+	private Statement statement1 = null;
+	private Statement statement2 = null;
+	private Statement statement3 = null;
+	private Statement statement4 = null;
+	private Statement statement5 = null;
 
 // armazena os resultados
 	private ResultSet resultset = null;
-	// armazena as consultas
-	private Statement statement2 = null;
-
-	// armazena os resultados
+	private ResultSet resultset1 = null;
 	private ResultSet resultset2 = null;
+	private ResultSet resultset3 = null;
+	private ResultSet resultset4 = null;
+	private ResultSet resultset5 = null;
 
 // cria a comboBox	
 	JComboBox comboBox;
@@ -58,7 +64,7 @@ public class conas {
 		String usuario = "root";
 
 // senha
-		String senha = "Aluno";
+		String senha = "arley911";
 
 // local driver instalado
 		String driver = "com.mysql.cj.jdbc.Driver";
@@ -91,6 +97,9 @@ public class conas {
 	private JFormattedTextField veiin;
 	private JFormattedTextField veiout;
 	private JTextArea acenome;
+	private JTextField ascod2;
+	private JTextField veiin2;
+	private JTextField veiout2;
 
 // lança a APLICAÇÃO lança a APLICAÇÃO lança a APLICAÇÃO lança a APLICAÇÃO	
 	public static void main(String[] args) {
@@ -222,14 +231,6 @@ public class conas {
 		frame.getContentPane().add(comboBox);
 		comboBox.setSelectedItem("Veículos que tem o Acessório");
 		
-		
-		
-		
-		//##### baixo baixo baixo baixo baixo baixo baixo baixo baixo baixo baixo baixo
-		
-		
-		
-
 // linha ADICIONAR VEÍCULOS linha ADICIONAR VEÍCULOS linha ADICIONAR VEÍCULOS	
 		JLabel labadveias = new JLabel("Adicionar Veículo");
 		labadveias.setFont(new Font("Tahoma", Font.BOLD, 24));
@@ -262,104 +263,252 @@ public class conas {
 		veiout.setBounds(791, 291, 150, 35);
 		frame.getContentPane().add(veiout);
 
-// linha BOTÃO EXCLUIR linha BOTÃO EXCLUIR linha BOTÃO EXCLUIR linha BOTÃO EXCLUIR
-		JButton btnasexcluir = new JButton("Excluir");
-		btnasexcluir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String id = ascod.getText();
-				apagarContato(id);
-				acenome.setText("");
-
-				atualizarInformacoes2(ascod.getText());
-
-			}
-		});
-		btnasexcluir.setFont(new Font("Tahoma", Font.BOLD, 36));
-		btnasexcluir.setBounds(383, 367, 223, 53);
-		frame.getContentPane().add(btnasexcluir);
-
-// linha BOTÂO VOLTAR linha BOTÂO VOLTAR linha BOTÂO VOLTAR linha BOTÂO VOLTAR
-		JButton btnasvoltar = new JButton("Voltar");
-		btnasvoltar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				conini tel = new conini();
-				tel.visivel();
-				frame.dispose();
-			}
-		});
-		btnasvoltar.setFont(new Font("Tahoma", Font.BOLD, 36));
-		btnasvoltar.setBounds(718, 367, 223, 53);
-		frame.getContentPane().add(btnasvoltar);
-	
-
 // linha BOTÃO SALVAR linha BOTÃO SALVAR linha BOTÃO SALVAR	 linha BOTÃO SALVAR	
 		JButton btnassalvar = new JButton("Salvar");
 		btnassalvar.setFont(new Font("Tahoma", Font.BOLD, 36));
 		btnassalvar.setBounds(45, 367, 223, 53);
 		frame.getContentPane().add(btnassalvar);
-		btnassalvar.addActionListener(new ActionListener());
-		botaosalvar(ascod.getText());
+		btnassalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String ascod2 = ascod.getText();
+				String veiin2 = veiin.getText();
+				String veiout2 = veiout.getText();
+				botaosalvar(veiin2, veiout2, ascod2);
+			}
+		});
 		
-}
+// linha BOTÃO EXCLUIR ACESSÓRIO linha BOTÃO EXCLUIR ACESSÓRIO linha BOTÃO EXCLUIR ACESSÓRIO
+		JButton btnasexcluir = new JButton("Excluir");
+		btnasexcluir.setFont(new Font("Tahoma", Font.BOLD, 36));
+		btnasexcluir.setBounds(383, 367, 223, 53);
+		frame.getContentPane().add(btnasexcluir);
+		btnasexcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent i) {
+				String ascod2 = ascod.getText();
+				botaoexcluir(ascod2);
+				}
+			});
+			public void actionPerformed(ActionEvent m) {
+				conini tel = new conini();
+				tel.visivel();
+				frame.dispose();
+			};
+		
+// linha BOTÂO VOLTAR linha BOTÂO VOLTAR linha BOTÂO VOLTAR linha BOTÂO VOLTAR
+		JButton btnasvoltar = new JButton("Voltar");
+		btnasvoltar.setFont(new Font("Tahoma", Font.BOLD, 36));
+		btnasvoltar.setBounds(718, 367, 223, 53);
+		frame.getContentPane().add(btnasvoltar);
+		btnasvoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent n) {
+				conini tel = new conini();
+				tel.visivel();
+				frame.dispose();
+			}
+		});
+	}
 	
+// inicio ação botao salvar inicio ação botao salvar inicio ação botao salvar inicio ação botao salvar inicio ação botao salvar
+		
 // AÇÕES BOTÃO SALVAR AÇÕES BOTÃO SALVAR AÇÕES BOTÃO SALVAR AÇÕES BOTÃO SALVAR AÇÕES BOTÃO SALVAR	
-		public void botaosalvar(String veiin, String veiout, String ace_id, String vei_id) {
-			// Passo 0 - Conferir se 'veiace_id' existe
-			if (ascod.getText() != null && ascod.getText() != 0) {
-				String queryx1 = "Select ace_id from acessorios where ace_id = "+ascod+"";
-				if (this.resultset.next()) {
+	public void botaosalvar(String ascod2, String veiin2, String veiout2) {
+		
+// salvar alterações de acessorios
+		if (ascod2.getText() != null && ascod2.getText() !=0) {
+			String queryupaces = "update acessorio set ace_nome = '"+acenome+"' where ace_id = '"+ascod2+"'";
+			this.statement.executeUpdate(queryupaces);	
+		};
+
+// incluir veiculo na ligação		
+		if (veiin2.getText() != null && veiin2.getText() != 0) {
+			String queryVeiin = "Select vei_id from veiculo where vei_id = "+veiin2+"";
+		    this.resultset1 = this.statement1.executeQuery(queryVeiin);
+		    if (this.resultset1.next()) {
+		    	chaveace = veiin2 + ascod2;
+		    	String querychacesexiste = "Select ace_id from ligacao where ace_id = "+chaveace+"";
+			    this.resultset2 = this.statement2.executeQuery(querychacesexiste);
+			    if (this.resultset2.next()) {
+			    	break;
+			    }else {
+			    	String queryinlig = "insert into acessorio (as_id) values '"+chaveace+"'";
+			    	this.statement3.executeUpdate(queryinlig);
+			    }
+		    }
+		}
+		
+// excluir veiculo da ligação
+		if (veiout2.getText() != null && veiout2.getText() != 0) {
+			String queryVeiin = "Select vei_id from veiculo where vei_id = "+veiout2+"";
+		    this.resultset3 = this.statement4.executeQuery(queryVeiin);
+		    if (this.resultset3.next()) {
+		    	chaveace2 = veiout2 + ascod2;
+		    	String querychacesexiste = "Select ace_id from ligacao where ace_id = "+chaveace2+"";
+			    this.resultset4 = this.statement5.executeQuery(querychacesexiste);
+			    if (this.resultset4.next()) {
+			    	break;
+			    }else {
+			    	String queryinlig = "delete into acessorio (as_id) values '"+chaveace2+"'";
+			    	this.statement6.executeUpdate(queryinlig);
+			    }
+		    }
+		}
+	}
+	
+// fim ação botao salvar fim ação botao salvar fim ação botao salvar fim ação botao salvar fim ação botao salvar
+		    	
+
+	
+// inicio ação botao excluir inicio ação botao excluir inicio ação botao excluir inicio ação botao excluir
+
+// AÇÕES BOTÃO EXCLUIR AÇÕES BOTÃO EXCLUIR AÇÕES BOTÃO EXCLUIR AÇÕES BOTÃO EXCLUIR AÇÕES BOTÃO EXCLUIR	
+	public void botaoexcluir(String ascod2) {
+			
+// verificar se acessorio existe e começar outra public void
+	if (ascod2.getText() != null && ascod2.getText() !=0) {
+		String queryaceexiste = "Select ace_id from veiculo where ace_id = "+ascod2+"";
+	    this.resultset = this.statement.executeQuery(queryaceexiste);
+	    if (this.resultset.next()) {
+	    	String queryexcace = "delete ace_id from acessorio where ace_id ='"+ascod2+"'";
+			this.statement1.executeUpdate(queryexcace);
+			verificaligacao(ascod2);			
+	    }else {
+	    	break;
+	    }
+	}
+	
+// verifica veiculos que tem o acessorio e apaga dos registros veiace
+	public void verificaligacao(String ascod2) {
+		try {
+			String queryVeiAce2 = "Select veiace_id from acessorios where RIGHT(veiace_id, 4) = '" + id + "'";
+			this.resultset3 = this.statement3.executeQuery(queryVeiAce);
+			// Percorrendo todos os registros no banco de dados "ligacao"
+			while (this.resultset3.next()) {
+				// Obtendo a chave "veiace_id"
+				String veiaceid = this.resultset3.getString("veiace_id");
+
+				// Dividindo a chave "veiace_id" em dois grupos de 4 dígitos
+				String grupo1 = veiaceid.substring(0, 4); // Primeiro grupo (vei_id)
+				String grupo2 = veiaceid.substring(4); // Segundo grupo (ace_id)
+
+				if(ascod2 = grupo2) {
+					String queryapagar = "delete ace_id from acessorios where ace_id ='"+id+"'";
+					this.statement5.executeUpdate(queryapagar);
+				};
+			} catch (Exception n) {
+				System.out.println("ERROR: " + n.getMessage());
+			}
+		}
+	}
+		
+		/*
+				String queryupaces = "update acessorio set ace_nome = '"+acenome+"' where ace_id = '"+ascod2+"'";
+				this.statement.executeUpdate(queryupaces);	
+			};
+
+			
+			
+			
+			
+		    	consultar veiace se existe
+		    	
+salvar veiace
+			
+				if (veiout.getText() != null && veiou.getText() != 0) {
 					break;
+				}
+			}else {
+	
+	String queryveicexiste = "Select ace_id from acessorios where ace_id = "+ascod+"";
+		if (this.resultset.next()) {
+			
+			// Passo 1 - Conferir se 'veiin' tem algum valor e se é diferente de 0 ou null
+			
+		    	
+		// Começo do LOOP
+	// Se o veiculo existe		
+		    		int chave1 = Integer.parseInt(vei_id+ace_id);
+		    		String queryacevei = "Select veiace_id from veiace where veiace_id = "+chave1+"";
+				    		this.resultset2 = this.statement.executeQuery(queryacevei);
+				    		if (this.resultset2.next()) {
+				    		}else {
+				    			String query = "insert into veiace (veiace_id) values('" +chave1+ "')";
+				    			this.statement.executeUpdate(query);
+				    		}
+				    	}else {
+				    		// passo 2 - Conferir se 'veiout' tem algum valor e se é diferente de 0 ou null
+						    if (veiout.getText() != null && veiout.getText() != 0) {
+						    	break;
+						    }else {
+						    	String queryVeiout = "Select vei_id from veiculo where vei_id = "+veiout+"";
+						    	this.resultset = this.statement.executeQuery(queryVeiout);
+						    	if (this.resultset.next()) {
+						    		int chave2 = Integer.parseInt(vei_id+ace_id);
+						    		String queryacevei1 = "Select veiace_id from veiace where veiace_id = "+chave2+"";
+						    		this.resultset = this.statement.executeQuery(queryacevei1);
+						    		if (this.resultset2.next()) {
+						    			String querya = "delete into veiace (veiace_id) values('" +chave2+ "')";
+						    			this.statement.executeUpdate(querya);
+				    	}
+					
+				
+// **************************		/
+			/*			    		
 				}else {
 					public void actionPerformed(ActionEvent e) {
 					conini tel = new conini();
 					tel.visivel();
 					frame.dispose();
 					}
-				}
+					
+					
+				
+				// Passo 1 - Conferir se 'veiin' tem algum valor e se é diferente de 0 ou null
+				if (veiin.getText() != null && veiin.getText() != 0) {
+			    	break;
+			    }else {
+			    	String queryVeiin = "Select vei_id from veiculo where vei_id = "+veiin+"";
+			    	this.resultset = this.statement.executeQuery(queryVeiin);
+			    	if (this.resultset.next()) {
+			    		int chave1 = Integer.parseInt(vei_id+ace_id);
+			    		String queryacevei = "Select veiace_id from veiace where veiace_id = "+chave1+"";
+			    		this.resultset2 = this.statement.executeQuery(queryacevei);
+			    		if (this.resultset2.next()) {
+			    			break;
+			    		}else {
+			    			String query = "insert into veiace (veiace_id) values('" +chave1+ "')";
+			    			this.statement.executeUpdate(query);
+			    		}
+			    	}
+			    	
+			    	  // passo 2 - Conferir se 'veiout' tem algum valor e se é diferente de 0 ou null
+				    if (veiout.getText() != null && veiout.getText() != 0) {
+				    	break;
+				    }else {
+				    	String queryVeiout = "Select vei_id from veiculo where vei_id = "+veiout+"";
+				    	this.resultset = this.statement.executeQuery(queryVeiout);
+				    	if (this.resultset.next()) {
+				    		int chave2 = Integer.parseInt(vei_id+ace_id);
+				    		String queryacevei1 = "Select veiace_id from veiace where veiace_id = "+chave2+"";
+				    		this.resultset = this.statement.executeQuery(queryacevei1);
+				    		if (this.resultset2.next()) {
+				    			String querya = "delete into veiace (veiace_id) values('" +chave2+ "')";
+				    			this.statement.executeUpdate(querya);
+				    		}
+				    	}
+				    }
+			    }
+			    
+				
+				*/
+				
+			
+				
 						
-			// Passo 1 - Conferir se 'veiin' tem algum valor e se é diferente de 0 ou null
-			if (veiin.getText() != null && veiin.getText() != 0) {
-		    	break;
-		    }else {
-		    	String queryVeiin = "Select vei_id from veiculo where vei_id = "+veiin+"";
-		    	this.resultset = this.statement.executeQuery(queryVeiin);
-		    	if (this.resultset.next()) {
-		    		int chave1 = Integer.parseInt(vei_id+ace_id);
-		    		String queryacevei = "Select veiace_id from veiace where veiace_id = "+chave1+"";
-		    		this.resultset2 = this.statement.executeQuery(queryacevei);
-		    		if (this.resultset2.next()) {
-		    			break;
-		    		}else {
-		    			String query = "insert into veiace (veiace_id) values('" +chave1+ "')";
-		    			this.statement.executeUpdate(query);
-		    		}
-		    	}
-		    }
-		    
-		    // passo 2 - Conferir se 'veiout' tem algum valor e se é diferente de 0 ou null
-		    if (veiout.getText() != null && veiout.getText() != 0) {
-		    	break;
-		    }else {
-		    	String queryVeiout = "Select vei_id from veiculo where vei_id = "+veiout+"";
-		    	this.resultset = this.statement.executeQuery(queryVeiout);
-		    	if (this.resultset.next()) {
-		    		int chave2 = Integer.parseInt(vei_id+ace_id);
-		    		String queryacevei1 = "Select veiace_id from veiace where veiace_id = "+chave2+"";
-		    		this.resultset = this.statement.executeQuery(queryacevei1);
-		    		if (this.resultset2.next()) {
-		    			String querya = "delete into veiace (veiace_id) values('" +chave2+ "')";
-		    			this.statement.executeUpdate(querya);
-		    		}
-		    	}
-		    }				
-		}		
-		String queryaceal = "update acessorio set ace_nome = '"+ace_nome+"' where ace_id = '"+ace_id+"'";
-		this.statement.executeUpdate(queryaceal);
-	}
 
-	
-	
-	
+// fim ação botao excluir fim ação botao excluir fim ação botao excluir fim ação botao excluir	
+
+						    		
+// inicio comboBox veiculos com acessorios inicio comboBox veiculos com acessorios
 // parte 1		
 // 1 - Consultando o banco de dados	ACESSORIOS
 	public void atualizarInformacoes(String id) {
@@ -415,54 +564,5 @@ public class conas {
 			System.out.println("ERROR: " + e.getMessage());
 		}
 	}
-
-
-
-// EDITAR DADOS EDITAR DADOS EDITAR DADOS EDITAR DADOS EDITAR DADOS EDITAR DADOS
-	public void editarContato(String ace_id, String ace_nome) {
-		try {
-
-// aspas simple que � usado no mysql
-			String query = "update acessorios set ace_nome ='" + ace_nome + "' where ace_id = '" + ace_id + "'";
-			this.statement.executeUpdate(query);
-		} catch (Exception e) {
-			System.out.println("ERROR: " + e.getMessage());
-		}
-	}
-
-// APAGAR ACESSORIO APAGAR ACESSORIO APAGAR ACESSORIO APAGAR ACESSORIO APAGAR ACESSORIO
-	public void apagarContato(String id) {
-		try {
-//aspas simple que � usado no mysql
-			String query = "delete from acessorios where ace_id ='" + id + "'";
-			this.statement.executeUpdate(query);
-		} catch (Exception e) {
-			System.out.println("ERROR: " + e.getMessage());
-		}
-	}
-
-// CADASTRAR OU ALTERAR O ACESSORIO CADASTRAR OU ALTERAR O ACESSORIO	
-	public boolean verificarContato(String id) {
-		try {
-			String query = "Select * from acessorio where ace_id = '" + id + "'";
-			this.resultset = this.statement.executeQuery(query);
-			while (this.resultset.next()) {
-				return true;
-			}
-		} catch (Exception e) {
-			System.out.println("ERROR: " + e.getMessage());
-		}
-		return false;
-	}
-
-	public void inserirContato(String id, String nome) {
-		try {
-
-//aspas simple que � usado no mysql
-			String query = "insert into acessorios(ace_id,ace_nome) values ('" + id + "','" + nome + "')";
-			this.statement.executeUpdate(query);
-		} catch (Exception e) {
-			System.out.println("ERROR: " + e.getMessage());
-		}
-	}
+// fim comboBox veiculos com acessorios fim comboBox veiculos com acessorios
 }
